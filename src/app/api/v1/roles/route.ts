f72973c0ab16dac7ed42430cc65ebe574e2696ad
Narongwas <darkwar008@gmail.com>
2025-06-21 18:31:33 +0700

    feat: add method to get and change user role

---
+++ src/app/api/v1/roles/route.ts
@@ -0,0 +1,76 @@
+import { db } from "@/utils/firebase.admin";
+
+export async function GET(request: Request) {
+  try {
+    const body = await request.json();
+    const { email } = body;
+
+    if (!email) {
+      return Response.json({ error: "Email is required" }, { status: 400 });
+    }
+
+    const studentId = parseInt(email.split("@")[0]);
+    const doc = await db
+      .collection("users")
+      .where("studentId", "==", studentId)
+      .get();
+
+    if (doc.empty) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const userDoc = doc.docs[0];
+
+    return Response.json(
+      {
+        email: userDoc.data().email,
+        role: userDoc.data().role,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    console.log("Error fetching user role:", e);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export async function PATCH(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, role } = body;
+
+    if (!email || !role) {
+      return Response.json(
+        { error: "Email and role are required" },
+        { status: 400 }
+      );
+    }
+
+    const studentId = parseInt(email.split("@")[0]);
+    const doc = await db
+      .collection("users")
+      .where("studentId", "==", studentId)
+      .get();
+
+    if (doc.empty) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const userDoc = doc.docs[0];
+
+    if (role === userDoc.data().role) {
+      return Response.json(
+        { error: "Role is already set to this value" },
+        { status: 400 }
+      );
+    }
+
+    await userDoc.ref.update({ role });
+    return Response.json(null, { status: 204 });
+  } catch (e) {
+    console.error("Error updating role:", e);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
