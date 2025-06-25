d3a7ce19236d7bd7078f9e034d5d7c57a1f92018
MildlyBrain <maethaphan.phai@gmail.com>
2025-06-26 01:44:23 +0700

    feat: add mock page for navbar

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/NavBar";
 
-export default function ExplorePage() {
+export default function GamePage() {
   return (
     <div className="flex h-screen flex-col content-center justify-center">
       <p className="font-bai type-headline-large text-center">Coming Soon...</p>
