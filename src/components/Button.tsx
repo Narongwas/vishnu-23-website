47fa2c64084000a5c719d233a315a0b83879193a
NlNJ4 <kkeaaphothi@gmail.com>
2025-06-26 22:02:28 +0700

    feat: button support icon, label, size, appearance

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,59 @@
-import React from "react";
-import cn from "@/lib/utils";
+import cn from "@/lib/helpers/cn";
+import Icon from "@/components/Icon";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  children: React.ReactNode;
-  className?: string;
+type ButtonSize = "Medium" | "Small" | "XSmall";
+type ButtonAppearance = "Primary" | "Secondary" | "Tertiary";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: string;
+  label?: string;
+  Size: ButtonSize;
+  Appearance: ButtonAppearance;
+}
+
+const SizeClasses: Record<ButtonSize, string> = {
+  Medium: "px-6 py-4 gap-3",
+  Small: "px-4 py-2.5 gap-3",
+  XSmall: "px-3 py-1.5 gap-2",
 };
 
-export default function Button({ children, className, ...props }: ButtonProps) {
+const IconSize: Record<ButtonSize, number> = {
+  Medium: 24,
+  Small: 20,
+  XSmall: 20,
+};
+
+const AppearanceClasses: Record<ButtonAppearance, string> = {
+  Primary: "bg-red text-white",
+  Secondary: "bg-yellow text-red",
+  Tertiary: "bg-white text-red",
+};
+
+export default function Button({
+  icon,
+  label,
+  Size,
+  Appearance,
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <button
       className={cn(
-        "flex justify-center items-center px-4 py-2.5 gap-3",
+        "flex items-center justify-center gap-3 px-4 py-2.5",
+        Size && SizeClasses[Size],
+        Appearance && AppearanceClasses[Appearance],
         className
       )}
+      type="button"
       {...props}
     >
-      {children}
+      {icon && (
+        <span className="-mx-1 flex flex-col items-center justify-center">
+          <Icon name={icon} size={IconSize[Size]} />
+        </span>
+      )}
+      {label && <span className="type-title-medium">{label}</span>}
     </button>
   );
 }
