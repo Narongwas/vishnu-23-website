58d9889e29dbe9fdb5e097b92cf79b5d05ce6854
NlNJ4 <kkeaaphothi@gmail.com>
2025-06-28 13:44:40 +0700

    fix: change a little style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
-import cn from "@/lib/helpers/cn";
 import Icon from "@/components/Icon";
+import cn from "@/lib/helpers/cn";
+import { StyleableFC } from "@/lib/types/misc";
 
 type ButtonSize = "Medium" | "Small" | "XSmall";
 type ButtonAppearance = "Primary" | "Secondary" | "Tertiary";
@@ -29,14 +30,15 @@ const AppearanceClasses: Record<ButtonAppearance, string> = {
   Tertiary: "bg-white text-red",
 };
 
-export default function Button({
+const Button: StyleableFC<ButtonProps> = ({
   icon,
   label,
   Size,
   Appearance,
   className,
+  style,
   ...props
-}: ButtonProps) {
+}) => {
   return (
     <button
       className={cn(
@@ -45,6 +47,7 @@ export default function Button({
         Appearance && AppearanceClasses[Appearance],
         className
       )}
+      style={style}
       type="button"
       {...props}
     >
@@ -56,4 +59,6 @@ export default function Button({
       {label && <span className="type-title-medium">{label}</span>}
     </button>
   );
-}
+};
+
+export default Button;
