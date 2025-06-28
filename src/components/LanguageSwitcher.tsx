1ed47e17de1f9600fb8123897f6bfa71218c103d
NlNJ4 <100306032+NlNJ4@users.noreply.github.com>
2025-06-29 01:23:59 +0700

    feat: add switch language logic and middleware

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import Button from "@/components/Button";
+import { useLocale, useTranslations } from "next-intl";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function LanguageSwitcher() {
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+  const t = useTranslations("LanguageSwitcher");
+  const nextLocale = locale === "th" ? "en" : "th";
+  function handleSwitch() {
+    const segments = pathname.split("/");
+
+    segments[1] = nextLocale;
+
+    router.push(segments.join("/"));
+  }
+
   return (
     <Button
       icon="language"
-      label="TH"
+      label={t("label")}
       Size="Small"
       Appearance="Tertiary"
-      aria-label="change language to English"
-      title="change language to English"
+      aria-label={t("alt")}
+      title={t("alt")}
+      onClick={handleSwitch}
     />
   );
 }
