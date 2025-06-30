5b3bf98489a291240ccabb89888ac92bfb4b01cb
Palm •-• <ch.enderpalm@gmail.com>
2025-06-30 21:28:03 +0700

    fix: missing NextRequest import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
