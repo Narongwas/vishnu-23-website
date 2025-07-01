6323605f0971dc0aa95ba49a494f3e1157294c80
Narongwas <darkwar008@gmail.com>
2025-07-01 14:39:55 +0700

    docs: add some comments

--- src/app/api/v1/friends/qr/decodeQR/route.ts
+++ src/app/api/v1/friends/qr/decodeQR/route.ts
@@ -4,6 +4,7 @@ import { firebaseAdmin } from "@/lib/services/firebase.admin";
 // This is a GET method to decode token from QR code and return the userID
 export async function GET(request: NextRequest) {
   try {
+    //get the token from the request headers or cookies
     const token =
       request.headers.get("token")?.split(" ")[1] ||
       request.cookies.get("token")?.value;
@@ -12,9 +13,11 @@ export async function GET(request: NextRequest) {
       return NextResponse.json({ error: "Invalid token" }, { status: 400 });
     }
 
+    // Verify the token and get the user ID
     const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
     const uid = decodedToken.uid;
 
+    // return the user ID
     return NextResponse.json(
       {
         uid: uid,
--- src/app/api/v1/friends/qr/route.ts
+++ src/app/api/v1/friends/qr/route.ts
@@ -2,9 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { db, firebaseAdmin } from "@/lib/services/firebase.admin";
 import QRCode from "qrcode";
 
+// Function to generate a random 5-digit code
 const generateCode = () => Math.floor(10000 + Math.random() * 90000).toString();
 
+//this is a GET method to generate a QR and 5-digit code for adding friends
 export async function GET(request: NextRequest) {
+  // Check if the request is authenticated
   try {
     const token =
       request.headers.get("Authorization")?.split(" ")[1] ||
@@ -14,14 +17,18 @@ export async function GET(request: NextRequest) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
+    // Verify the token and get the user ID
     const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
     const uid = decodedToken.uid;
 
+    // Generate a random 5-digit code
     const code = generateCode();
 
+    //get user name from user document
     const user = await db.collection("users").doc(uid).get();
     const name = user.data()?.name;
 
+    //generate a QR with the user's name and token
     const qrDataUrl = await QRCode.toDataURL(
       JSON.stringify({
         name,
@@ -29,10 +36,12 @@ export async function GET(request: NextRequest) {
       })
     );
 
+    //add the code to the user's document in Firestore
     await user.data()?.ref.update({
       addFriendCode: code,
     });
 
+    // Return the QR code and the 5-digit code
     return NextResponse.json(
       {
         qrcode: qrDataUrl,
