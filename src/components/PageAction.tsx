56a5f9bde4359ecaa8bd3abe7190167acdb9b754
PanisaBD <panisa2548@gmail.com>
2025-07-04 13:38:22 +0700

    refactor: add icon classname to prop

--- src/components/PageAction.tsx
+++ src/components/PageAction.tsx
@@ -1,14 +1,17 @@
 import cn from "@/lib/helpers/cn";
-import { ReactNode, ButtonHTMLAttributes } from "react";
+import Icon from "@/components/Icon";
+import { ButtonHTMLAttributes } from "react";
 
 type PageActionProps = {
-  icon?: ReactNode;
+  icon?: string;
+  iconClassName?: string;
   label?: string;
   labelClassName?: string;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const PageAction = ({
+const PageAction = ({
   icon = "person_add",
+  iconClassName,
   label = "เพิ่มเพื่อน",
   labelClassName,
   className,
@@ -19,15 +22,20 @@ export const PageAction = ({
     <button
       type="button"
       className={cn(
-        "relative flex w-[402px] flex-col items-center gap-3 overflow-hidden px-[181px] pt-12 pb-8",
+        "relative flex w-[402px] flex-col items-center gap-[12px] overflow-hidden px-[181px] pt-12 pb-8",
         className
       )}
       style={style}
       {...props}
     >
       <div className="bg-yellow absolute top-4 left-[-166px] h-[540px] w-[734px] rounded-[367px/270px]" />
-      <div className="text-blue relative mt-[-1px] w-fit text-4xl whitespace-nowrap">
-        {icon}
+      <div
+        className={cn(
+          "text-blue relative mt-[-1px] w-fit text-4xl",
+          iconClassName
+        )}
+      >
+        <Icon name={icon} size={40} />
       </div>
       <div
         className={cn(
@@ -40,3 +48,5 @@ export const PageAction = ({
     </button>
   );
 };
+
+export default PageAction;
