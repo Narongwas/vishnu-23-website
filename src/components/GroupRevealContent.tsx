879869d2c24587e2d7a9ded04ae556baa5f92a09
frostnzx <jeensiwakorn001@gmail.com>
2025-07-05 00:07:32 +0700

    fix: fix group-reveal page to use getServerAuth

---
+++ src/components/GroupRevealContent.tsx
@@ -0,0 +1,29 @@
+import { getServerAuth } from "@/lib/firebase/getServerAuth";
+
+export default async function GroupReveal() {
+  const { token } = await getServerAuth();
+
+  if (!token) {
+    return <div>You must be logged in to view your group.</div>;
+  }
+
+  const res = await fetch(`${process.env.API_BASE_URL}/api/v1/group`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return <div>Failed to fetch group info</div>;
+  }
+
+  const data = await res.json();
+
+  return (
+    <div>
+      Congratulations your group is <br />
+      Group : {data.group}
+    </div>
+  );
+}
