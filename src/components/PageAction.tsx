a9d2d2eb56bf810e9a5172dc2b16ebc8306936df
PanisaBD <panisa2548@gmail.com>
2025-07-04 13:16:17 +0700

    fix: change props receipt for page action

--- src/components/PageAction.tsx
+++ src/components/PageAction.tsx
@@ -1,40 +1,42 @@
-"use client";
+import cn from "@/lib/helpers/cn";
+import { ReactNode, ButtonHTMLAttributes } from "react";
 
-interface PageActionProps {
-  icon: string;
-  text: string;
-  onClick?: () => void;
-}
+type PageActionProps = {
+  icon?: ReactNode;
+  label?: string;
+  labelClassName?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function PageAction({ icon, text, onClick }: PageActionProps) {
+export const PageAction = ({
+  icon = "person_add",
+  label = "เพิ่มเพื่อน",
+  labelClassName,
+  className,
+  style,
+  ...props
+}: PageActionProps) => {
   return (
     <button
-      onClick={onClick}
       type="button"
-      className="
-        fixed bottom-0 left-1/2
-        w-full max-w-md h-[140px]
-        flex flex-col items-center justify-center
-        p-6 z-10 overflow-visible border-0 bg-none cursor-pointer text-center
-        transform -translate-x-1/2
-      "
+      className={cn(
+        "relative flex w-[402px] flex-col items-center gap-3 overflow-hidden px-[181px] pt-12 pb-8",
+        className
+      )}
+      style={style}
+      {...props}
     >
-      <div
-        className="
-          absolute bottom-0 left-1/2
-          w-[140vw] h-[270px]
-          rounded-t-[1000px]
-          -translate-x-1/2 translate-y-3/7
-          bg-yellow
-          -z-10
-        "
-      />
-
-      <div className="font-icon text-[2.25rem] font-normal leading-[2.5rem] z-10 text-blue">
+      <div className="bg-yellow absolute top-4 left-[-166px] h-[540px] w-[734px] rounded-[367px/270px]" />
+      <div className="text-blue relative mt-[-1px] w-fit text-4xl whitespace-nowrap">
         {icon}
       </div>
-
-      <div className="font-bold text-base leading-6 z-10 text-blue">{text}</div>
+      <div
+        className={cn(
+          "type-title-medium text-blue relative w-fit text-center whitespace-nowrap",
+          labelClassName
+        )}
+      >
+        {label}
+      </div>
     </button>
   );
-}
+};
