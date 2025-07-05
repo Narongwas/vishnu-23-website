a82c27f50ee9eadce8a7f945b387a593a446c514
frostnzx <jeensiwakorn001@gmail.com>
2025-07-05 16:44:06 +0700

    fix: login button to directly login without login page

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,65 @@
+"use client";
 import Button from "@/components/Button";
-import { useTranslations } from "next-intl";
+import { useAuth } from "@/lib/contexts/AuthContext";
+import { signInWithGoogle } from "@/lib/firebase/auth";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-export default function LoginButton() {
-  const t = useTranslations("HomeHero");
+interface GoogleLoginBtnProps {
+  onSuccess?: () => void;
+  onError?: (error: string) => void;
+  className?: string;
+}
 
-  return (
+export default function GoogleLoginBtn({
+  onSuccess,
+  onError,
+}: GoogleLoginBtnProps) {
+  const { loginWithToken, token, user } = useAuth();
+  const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (token != null) {
+      setLoggedIn(true);
+    }
+  }, [token]);
+
+  console.log("photo : " + user?.photoURL);
+
+  const handleGoogleLogin = async () => {
+    try {
+      const { user, error } = await signInWithGoogle();
+
+      if (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : JSON.stringify(error);
+        onError?.(errorMessage || "Login failed");
+      }
+      if (user) {
+        console.log("GoogleLoginButton - User signed in:", user.email);
+        await loginWithToken(user);
+        setLoggedIn(true);
+        onSuccess?.();
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      onError?.((error as unknown as string) || "Login failed");
+    }
+  };
+
+  return isLoggedIn ? (
+    <>
+      <Image
+        src={user?.photoURL ?? ""}
+        alt=""
+        width={48}
+        height={48}
+        className="rounded-full border border-gray-300"
+      />
+    </>
+  ) : (
     <Button
+      onClick={handleGoogleLogin}
       icon="login"
       label={t("action.logIn")}
       Size="Small"
