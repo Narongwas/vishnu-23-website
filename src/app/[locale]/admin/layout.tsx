e62f1424bced42af9ace4176a6e89279a1005be7
nuna101 <nunajidapanak@gmail.com>
2025-07-06 20:56:48 +0700

    fix: admin layout

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/[locale]/globals.css";
-import cn from "@/lib/helpers/cn";
-import type { Viewport } from "next";
 import BackgroundWithNoise from "@/components/BackgroundWithNoise";
+import type { Viewport } from "next";
 
 export const viewport: Viewport = {
   themeColor: "#f4d590",
@@ -13,12 +12,8 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("antialiased")}>
-        <BackgroundWithNoise className="from-yellow to-yellow-white bg-gradient-to-b">
-          <div className="relative mx-auto max-w-200">{children}</div>
-        </BackgroundWithNoise>
-      </body>
-    </html>
+    <BackgroundWithNoise className="from-yellow to-yellow-white bg-gradient-to-b">
+      <div className="relative mx-auto max-w-200">{children}</div>
+    </BackgroundWithNoise>
   );
 }
