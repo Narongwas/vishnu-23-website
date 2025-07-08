8d51c253e14e32a77a40e0fd1df012b2b092a1d9
Narongwas <darkwar008@gmail.com>
2025-07-08 22:46:15 +0700

    refactor: get prediction score leaderboard

--- src/app/api/v1/prediction/score/route.ts
+++ src/app/api/v1/prediction/score/route.ts
@@ -2,44 +2,70 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/services/firebase.admin";
 
 export async function GET(request: NextRequest) {
-  const predictionId = request.nextUrl.searchParams.get("prediction");
+  try {
+    const predictionId = request.nextUrl.searchParams.get("prediction");
 
-  if (!predictionId) {
+    if (!predictionId) {
+      return NextResponse.json(
+        { error: "prediction ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const [prediction, answersData, groupsSnap, usersSnap] = await Promise.all([
+      db.collection("prediction").doc(predictionId).get(),
+      db
+        .collection("answer")
+        .where("predictionId", "==", predictionId)
+        .where("isCorrect")
+        .select("userId", "isCorrect")
+        .get(),
+      db.collection("group").get(),
+      db.collection("user").get(),
+    ]);
+
+    const groups = groupsSnap.docs.map((doc) => {
+      return { group: doc.id, name: doc.data().name };
+    });
+
+    const userData = usersSnap.docs;
+
+    const groupsList = groups.map(({ group, name }) => {
+      const usersInGroup = userData
+        .filter((userDoc) => userDoc.data().group === group)
+        .map((userDoc) => userDoc.id);
+      const userCorrect = answersData.docs.filter((answerDoc) =>
+        usersInGroup.includes(answerDoc.data().userId)
+      );
+
+      return {
+        group,
+        name,
+        correctCount: userCorrect.length,
+      };
+    });
+
+    groupsList.sort((a, b) => b.correctCount - a.correctCount);
+
     return NextResponse.json(
-      { error: "prediction ID is required" },
-      { status: 400 }
+      {
+        question: prediction.data()?.question,
+        day: prediction.data()?.day,
+        time: prediction.data()?.time,
+        leaderboard: groupsList,
+      },
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (err) {
+    console.error("Error fetching prediction leaderboard : " + err);
+    return NextResponse.json(
+      { error: "Error fetching prediction leaderboard" },
+      { status: 500 }
     );
   }
-
-  const answersData = await db
-    .collection("answer")
-    .where("predictionId", "==", predictionId)
-    .where("isCorrect")
-    .select("userId", "isCorrect")
-    .get();
-
-  const groups = (await db.collection("group").get()).docs.map((doc) => {
-    return { group: doc.id, name: doc.data().name };
-  });
-
-  const userData = (await db.collection("user").get()).docs;
-
-  const groupsList = groups.map(({ group, name }) => {
-    const usersInGroup = userData
-      .filter((userDoc) => userDoc.data().group === group)
-      .map((userDoc) => userDoc.id);
-    const userCorrect = answersData.docs.filter((answerDoc) =>
-      usersInGroup.includes(answerDoc.data().userId)
-    );
-
-    return {
-      group,
-      name,
-      correctCount: userCorrect.length,
-    };
-  });
-
-  groupsList.sort((a, b) => b.correctCount - a.correctCount);
-
-  return groupsList;
 }
