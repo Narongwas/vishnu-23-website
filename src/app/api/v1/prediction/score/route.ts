687b9b758d746e25c88ce95be9bec96437140bb8
Narongwas <darkwar008@gmail.com>
2025-07-09 22:37:16 +0700

    chore: remove add score in prediction

--- src/app/api/v1/prediction/score/route.ts
+++ src/app/api/v1/prediction/score/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/services/firebase.admin";
-import { FieldValue } from "firebase-admin/firestore";
 
 //This is a method to get a list of group and number of member who is correct
 // in predictionId query
@@ -73,43 +72,6 @@ export async function GET(request: NextRequest) {
     console.error("Error fetching prediction leaderboard : " + err);
     return NextResponse.json(
       { error: "Error fetching prediction leaderboard" },
-      { status: 500 }
-    );
-  }
-}
-
-//This is a method to add group score, that has a highest member who is correct
-//Should send body like [{ group1 , point1},{ group2 , point2},{ group3, point3}]
-export async function PATCH(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const [first, second, third]: { group: string; point?: number }[] = body;
-
-    if (!first.group || !second.group || !third.group) {
-      return NextResponse.json(
-        { error: "List of first 3 groups is required" },
-        { status: 400 }
-      );
-    }
-
-    await db
-      .batch()
-      .update(db.collection("groups").doc(first.group), {
-        totalScore: FieldValue.increment(first.point ?? 0),
-      })
-      .update(db.collection("groups").doc(second.group), {
-        totalScore: FieldValue.increment(second.point ?? 0),
-      })
-      .update(db.collection("groups").doc(third.group), {
-        totalScore: FieldValue.increment(third.point ?? 0),
-      })
-      .commit();
-
-    return new NextResponse(null, { status: 204 });
-  } catch (err) {
-    console.error("Error adding score" + err);
-    return NextResponse.json(
-      { error: "Error adding score" + err },
       { status: 500 }
     );
   }
