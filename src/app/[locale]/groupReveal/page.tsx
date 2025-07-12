3abfc3dc34970b54723a15345bf532b3bb5ea293
NlNJ4 <100306032+NlNJ4@users.noreply.github.com>
2025-07-12 18:28:02 +0700

    feat: fetching group and mapping group

--- src/app/[locale]/groupReveal/page.tsx
+++ src/app/[locale]/groupReveal/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { checkFeatureFlagByName } from "@/app/api/v1/feature-flags/services";
-import GroupReveal from "@/components/GroupRevealContent";
+import GroupReveal from "@/app/[locale]/groupReveal/components/GroupRevealContent";
 
 export default async function Page() {
   const groupRevealFlag = await checkFeatureFlagByName("groupReveal");
