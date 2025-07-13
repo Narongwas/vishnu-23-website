42848bfb0364034426d438be5585ff20a7176cf2
nuna101 <nunajidapanak@gmail.com>
2025-07-13 21:13:40 +0700

    fix: edit styles

--- src/components/HomeWrapper.tsx
+++ src/components/HomeWrapper.tsx
@@ -1,13 +1,13 @@
 import BackgroundWithNoise from "@/components/BackgroundWithNoise";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
+import LoginButton from "@/components/LoginButton";
+import NavBar from "@/components/NavBar";
 import cn from "@/lib/helpers/cn";
 import cloud1Logo from "@/public/decorating/clouds/cloud1.svg";
 import cloud2Logo from "@/public/decorating/clouds/cloud2.svg";
 import firstDateLogo from "@/public/logo/firstdate.svg";
-import Image from "next/image";
-import LanguageSwitcher from "@/components/LanguageSwitcher";
-import LoginButton from "@/components/LoginButton";
-import NavBar from "@/components/NavBar";
 import vishnuLogo from "@/public/logo/vishnu.svg";
+import Image from "next/image";
 
 export default function HomeWrapper({
   children,
@@ -21,11 +21,11 @@ export default function HomeWrapper({
     <BackgroundWithNoise
       className={cn("from-yellow to-yellow-white bg-gradient-to-b", classname)}
     >
-      <div className="relative z-10 flex w-full items-center justify-between p-4 lg:justify-around">
+      <div className="pt- absolute z-10 flex w-full items-center justify-between p-4">
         <LanguageSwitcher />
         <LoginButton />
       </div>
-      <div className="relative mx-auto flex h-full max-w-200 flex-col items-center text-center">
+      <div className="relative mx-auto flex h-full max-w-200 flex-col items-center pt-11 text-center">
         <Image
           src={cloud1Logo}
           width={73.5}
