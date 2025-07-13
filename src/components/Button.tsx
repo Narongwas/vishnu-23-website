75e7f13fab03ecca0a0641764157842c520b504c
nuna101 <nunajidapanak@gmail.com>
2025-07-13 21:48:50 +0700

    fix: update button with interactive

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,26 @@
-import Icon from "@/components/Icon";
 import cn from "@/lib/helpers/cn";
 import { StyleableFC } from "@/lib/types/misc";
+import Interactive from "@/components/Interactive";
 
 type ButtonSize = "Medium" | "Small" | "XSmall";
 type ButtonAppearance = "Primary" | "Secondary" | "Tertiary";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  icon?: string;
-  label?: string;
   Size: ButtonSize;
   Appearance: ButtonAppearance;
+  children: React.ReactNode;
 }
 
 const SizeClasses: Record<ButtonSize, string> = {
-  Medium: "px-6 py-4 gap-3",
+  Medium: "px-6 py-4 gap-4",
   Small: "px-4 py-2.5 gap-3",
   XSmall: "px-3 py-1.5 gap-2",
 };
 
-const IconSize: Record<ButtonSize, number> = {
-  Medium: 24,
-  Small: 20,
-  XSmall: 20,
+const iconMarginSelector: Record<ButtonSize, string[]> = {
+  Medium: ["[&>svg]:-mx-2", "[&>svg]:h-6", "[&>i]:-mx-2"],
+  Small: ["[&>svg]:-mx-1", "[&>svg]:h-4", "[&>i]:-mx-1"],
+  XSmall: ["[&>svg]:-mx-1.5", "[&>svg]:h-4", "[&>i]:-mx-1.5"],
 };
 
 const AppearanceClasses: Record<ButtonAppearance, string> = {
@@ -31,33 +30,33 @@ const AppearanceClasses: Record<ButtonAppearance, string> = {
 };
 
 const Button: StyleableFC<ButtonProps> = ({
-  icon,
-  label,
   Size,
   Appearance,
   className,
   style,
+  children,
   ...props
 }) => {
   return (
-    <button
+    <Interactive
+      type="button"
       className={cn(
-        "flex items-center justify-center",
-        Size && SizeClasses[Size],
-        Appearance && AppearanceClasses[Appearance],
+        "relative flex items-center justify-center",
+        SizeClasses[Size],
+        AppearanceClasses[Appearance],
+        iconMarginSelector[Size],
         className
       )}
-      style={style}
-      type="button"
+      style={{
+        backgroundImage: `url('/decorating/texture/fabric.png')`,
+        backgroundBlendMode: "soft-light",
+        backgroundSize: "cover",
+        ...style,
+      }}
       {...props}
     >
-      {icon && (
-        <span className="-mx-1 flex flex-col items-center justify-center">
-          <Icon name={icon} size={IconSize[Size]} />
-        </span>
-      )}
-      {label && <span className="type-title-medium">{label}</span>}
-    </button>
+      {children}
+    </Interactive>
   );
 };
 
