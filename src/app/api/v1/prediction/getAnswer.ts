f5a70facf5655d80df406f13631b8f717177375f
Narongwas <darkwar008@gmail.com>
2025-07-13 22:25:43 +0700

    feat: add parameter in getUserAnswer

--- src/app/api/v1/prediction/getAnswer.ts
+++ src/app/api/v1/prediction/getAnswer.ts
@@ -1,11 +1,18 @@
-export async function getUserAnswer() {
-  const response = await fetch("/api/v1/prediction/answer", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
+export async function getUserAnswer({
+  predictionId,
+}: {
+  predictionId: string;
+}) {
+  const response = await fetch(
+    `/api/v1/prediction/answer?prediction=${predictionId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch answer");
