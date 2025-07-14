42ceebf9ce14645c55fce149de520b532fb9342f
nuna101 <nunajidapanak@gmail.com>
2025-07-14 14:22:08 +0700

    fix: shift the clubs panel position upper

--- src/app/[locale]/explore/clubs/page.tsx
+++ src/app/[locale]/explore/clubs/page.tsx
@@ -11,7 +11,7 @@ export default function ClubPage() {
         <div className="absolute top-0 z-10 flex w-full items-center justify-between py-4">
           <BackButton variants="Tertiary" />
         </div>
-        <ClubPanel />
+        <ClubPanel className="-mt-16" />
       </div>
     </BackgroundWithNoise>
   );
