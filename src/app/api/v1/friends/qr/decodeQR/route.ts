f7bb21af9e1e55aa1ca7765107f87c40f8574c4f
Narongwas <darkwar008@gmail.com>
2025-07-16 02:43:56 +0700

    refactor: remove content-type application/json again

--- src/app/api/v1/friends/qr/decodeQR/route.ts
+++ src/app/api/v1/friends/qr/decodeQR/route.ts
@@ -34,7 +34,6 @@ export async function GET(request: NextRequest) {
       },
       {
         status: 200,
-        headers: { "Content-Type": "application/json" },
       }
     );
   } catch (e) {
--- src/app/api/v1/friends/route.ts
+++ src/app/api/v1/friends/route.ts
@@ -29,7 +29,6 @@ export async function GET(request: NextRequest) {
       },
       {
         status: 200,
-        headers: { "Content-Type": "application/json" },
       }
     );
   } catch (e) {
