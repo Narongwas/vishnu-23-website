f1a9a5af3b0b4a6ae81f8de0ac46b440625e09bb
nuna101 <nunajidapanak@gmail.com>
2025-07-16 23:12:08 +0700

    feat: admin feature menu page

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/[locale]/globals.css";
-import cn from "@/lib/helpers/cn";
-import type { Viewport } from "next";
 import BackgroundWithNoise from "@/components/BackgroundWithNoise";
+import type { Viewport } from "next";
 
 export const viewport: Viewport = {
   themeColor: "#f4d590",
@@ -13,10 +12,12 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("antialiased")}>
+    <html>
+      <body>
         <BackgroundWithNoise className="from-yellow to-yellow-white bg-gradient-to-b">
-          <div className="relative mx-auto max-w-200">{children}</div>
+          <div className="relative mx-auto min-h-screen max-w-200 px-4">
+            {children}
+          </div>
         </BackgroundWithNoise>
       </body>
     </html>
