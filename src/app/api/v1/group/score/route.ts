19e5d2ac8da041623c01a2eba9bbbdcbf7341786
frostnzx <jeensiwakorn001@gmail.com>
2025-07-16 17:05:31 +0700

    feat: add getAll group score

---
+++ src/app/api/v1/group/score/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/services/firebase.admin";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const snapshot = await db.collection("groups").get();
+
+    if (snapshot.empty) {
+      return NextResponse.json({ groups: [] }, { status: 200 });
+    }
+
+    const groups = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+      score: doc.data()?.totalScore || 0,
+    }));
+
+    return NextResponse.json({ groups }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Failed to get group scores: " + err },
+      { status: 500 }
+    );
+  }
+}
