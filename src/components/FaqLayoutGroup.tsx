6b227fc70286ada84a0dd0fab6b437f3d88f6e63
nuna101 <nunajidapanak@gmail.com>
2025-07-17 03:48:34 +0700

    feat: add english faqs in json

--- src/components/FaqLayoutGroup.tsx
+++ src/components/FaqLayoutGroup.tsx
@@ -8,19 +8,24 @@ import { LayoutGroup } from "motion/react";
 import { useTranslations } from "next-intl";
 
 const FaqLayoutGroup: StyleableFC = ({ className, style }) => {
-  const t = useTranslations("");
-  const faqs = t.raw("Faq") as FaqSection[];
+  const t = useTranslations("Home.Faq");
+  const faqs = t.raw("questions") as FaqSection[];
 
   return (
-    <div
-      className={cn("flex flex-col items-center gap-6", className)}
-      style={style}
-    >
-      <LayoutGroup>
-        {faqs.map((section) => (
-          <FaqGroup key={section.id} section={section} />
-        ))}
-      </LayoutGroup>
+    <div className="z-30 w-full pb-30">
+      <div className="type-title-large text-red mt-17 mb-5 text-center font-bold">
+        <p>{t("title")}</p>
+      </div>
+      <div
+        className={cn("flex flex-col items-center gap-6", className)}
+        style={style}
+      >
+        <LayoutGroup>
+          {faqs.map((section) => (
+            <FaqGroup key={section.id} section={section} />
+          ))}
+        </LayoutGroup>
+      </div>
     </div>
   );
 };
