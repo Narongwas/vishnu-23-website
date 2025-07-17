d0db9b9d10b54a11924384e77cc79df4f5992d4d
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-07-17 11:48:51 +0700

    fix: change get bingo scores to protected

--- src/app/api/v1/bingo/scores/route.ts
+++ src/app/api/v1/bingo/scores/route.ts
@@ -1,9 +1,24 @@
-import { db } from "@/lib/services/firebase.admin";
-import { NextResponse } from "next/server";
+import { db, firebaseAdmin } from "@/lib/services/firebase.admin";
+import { NextRequest, NextResponse } from "next/server";
 
-// GET : "api/v1/bingo/scores" public
+// GET : "api/v1/bingo/scores" protected
 // get all groups' bingoScore and sort them in descending order
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const token =
+    request.headers.get("Authorization")?.split(" ")[1] ||
+    request.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+  const role = decodedToken.role;
+
+  if (role !== "admin") {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const groupsSnapshot = await db
     .collection("groups")
     .orderBy("bingoScore", "desc")
