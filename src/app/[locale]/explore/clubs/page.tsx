25ab024b781ed0feb2829403345564cad25ce31e
nuna101 <nunajidapanak@gmail.com>
2025-07-19 02:24:14 +0700

    fix: adjust spacing

--- src/app/[locale]/explore/clubs/page.tsx
+++ src/app/[locale]/explore/clubs/page.tsx
@@ -12,7 +12,7 @@ export default async function ClubPage() {
       <div className="absolute top-0 z-10 flex w-full items-center justify-between py-4">
         <BackButton variants="tertiary" />
       </div>
-      <ClubPanel className="-mt-16 pb-10" />
+      <ClubPanel className="pt-8 pb-10" />
       <AllPageSponsorFooter />
     </>
   );
