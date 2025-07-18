67ee4bf4525f8acaccc51ee8062f39ef9e8e0a01
Siravit Phokeed <siravitsasa@gmail.com>
2025-07-18 22:15:39 +0700

    fix: change nav bar item state layer color

--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -2,6 +2,7 @@
 
 import Icon from "@/components/Icon";
 import Interactive from "@/components/Interactive";
+import cn from "@/lib/helpers/cn";
 import { usePathname } from "next/navigation";
 
 export default function NavBarItem({
@@ -21,7 +22,7 @@ export default function NavBarItem({
   return (
     <Interactive
       href={href}
-      className="flex flex-col items-center gap-1 py-1.5"
+      className="text-yellow-white flex flex-col items-center gap-1 py-1.5"
     >
       <div className="relative flex h-8 w-14 items-center justify-center">
         {isActive && (
@@ -33,10 +34,10 @@ export default function NavBarItem({
         <Icon
           name={icon}
           size={24}
-          className={isActive ? "text-red z-10" : "text-yellow-white z-10"}
+          className={cn("z-10", isActive && "text-red")}
         />
       </div>
-      <span className="type-label-medium text-yellow-white">{label}</span>
+      <span className="type-label-medium">{label}</span>
     </Interactive>
   );
 }
