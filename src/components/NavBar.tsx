764ae16a779ad1de812b51a87de14c3c03dfafe4
Siravit Phokeed <siravitsasa@gmail.com>
2025-07-18 21:48:10 +0700

    fix: nav bar item not fill nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
 
   return (
     <nav className="bg-red fixed bottom-0 left-0 z-40 flex h-16 w-full items-center px-4">
-      <div className="flex w-full flex-row justify-around">
+      <div className="flex w-full flex-row *:grow">
         <NavBarItem icon="home" label={t("home")} href="/" />
         <NavBarItem icon="explore" label={t("explore")} href="/explore" />
         <NavBarItem icon="sports_esports" label={t("games")} href="/games" />
