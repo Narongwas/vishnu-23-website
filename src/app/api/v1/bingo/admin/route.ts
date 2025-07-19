8a7d5fe9a468f7ae1b736dcb5a0337e4fa7e0b14
NlNJ4 <100306032+NlNJ4@users.noreply.github.com>
2025-07-19 15:03:07 +0700

    fix: error msg

--- src/app/api/v1/bingo/admin/route.ts
+++ src/app/api/v1/bingo/admin/route.ts
@@ -14,7 +14,7 @@ export async function PATCH(request: NextRequest) {
 
   // if staff token is not provided, return error
   if (!staffToken) {
-    return NextResponse.json({ error: "sda" }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   // verify the staff token
@@ -33,7 +33,7 @@ export async function PATCH(request: NextRequest) {
   }
 
   if (!isClubAdmin) {
-    return NextResponse.json({ error: "adwww1" }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   // get camper id from the query string
