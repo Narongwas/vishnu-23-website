5657010f44ba546337030bc5b8495c001a3f9633
MildlyBrain <maethaphan.phai@gmail.com>
2025-07-19 16:19:43 +0700

    fix: translations and navbar display state

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,29 @@
 import NavBarItem from "@/components/NavBarItem";
 import { getUserRole } from "@/lib/middleware/checkUserAccess";
+import { getTranslations } from "next-intl/server";
 
 export default async function NavBar() {
+  const t = await getTranslations("Common.Navigation");
   let role = await getUserRole();
   if (role == null) {
     role = "camper";
   }
-  // noted that I saw your useTranslation doesn't work both in prod and when im testing so I removed that
+
   return (
     <nav className="bg-red fixed bottom-0 left-0 z-40 flex h-16 w-full items-center px-4">
       <div className="flex w-full flex-row *:grow">
-        <NavBarItem icon="home" label={"home"} href="/" />
-        <NavBarItem icon="explore" label={"explore"} href="/explore" />
-        {/* <NavBarItem icon="sports_esports" label={t("games")} href="/games" /> */}
-        {role != "camper" && (
-          <NavBarItem icon="security" label={"admin"} href="/admin" />
+        <NavBarItem icon="home" label={t("home")} href="/" />
+        {role != "camper" ? (
+          <NavBarItem icon="security" label={t("admin")} href="/admin" />
+        ) : (
+          <>
+            <NavBarItem icon="explore" label={t("explore")} href="/explore" />
+            <NavBarItem
+              icon="sports_esports"
+              label={t("games")}
+              href="/games"
+            />
+          </>
         )}
       </div>
     </nav>
