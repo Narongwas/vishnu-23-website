ecb44ef70fcc96d03d098d2913293d94f421b10c
Narongwas <darkwar008@gmail.com>
2025-07-20 10:55:45 +0700

    refactor: remove header in score

--- src/app/api/v1/prediction/score/route.ts
+++ src/app/api/v1/prediction/score/route.ts
@@ -63,9 +63,6 @@ export async function GET(request: NextRequest) {
       },
       {
         status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
       }
     );
   } catch (err) {
