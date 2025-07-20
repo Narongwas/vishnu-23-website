65865553730065718f861b2aeab20bc5dd875a27
MildlyBrain <maethaphan.phai@gmail.com>
2025-07-20 18:58:46 +0700

    fix: add promise

--- src/app/api/v1/registrationPoint/[id]/route.ts
+++ src/app/api/v1/registrationPoint/[id]/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/services/firebase.admin";
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const studentId = params.id;
+  const studentId = (await params).id;
   const user = await db.collection("users").doc(studentId).get();
   if (!user.exists) {
     return NextResponse.json({ error: "Wrong student id" }, { status: 400 });
@@ -13,12 +13,5 @@ export async function GET(
   const userGroup = user.data()?.group;
   const group = await db.collection("groups").doc(userGroup).get();
   const registrationsPoint = group.data()?.registrationsPoint;
-  return NextResponse.json(
-    {
-      registrationsPoint,
-    },
-    {
-      status: 200,
-    }
-  );
+  return NextResponse.json({ registrationsPoint }, { status: 200 });
 }
