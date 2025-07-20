3f17f5f258ba5f624cac6263bc53d7ffab2e8e72
Narongwas <darkwar008@gmail.com>
2025-07-21 00:02:15 +0700

    feat: add protect route

--- src/app/api/v1/prediction/route.ts
+++ src/app/api/v1/prediction/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/services/firebase.admin";
 import { bilingualString } from "@/lib/types/bilingual";
+import { firebaseAuthMiddleware } from "@/lib/middleware/firebaseAuthMiddleware";
+import emailToId from "@/lib/helpers/emailToId";
+import scoreAdmin from "@/jsondata/score-admin.json";
 
 //this is a function to add new prediction
 async function addPrediction(prediction: {
@@ -21,8 +24,28 @@ async function addPrediction(prediction: {
 }
 
 //this is a method to get all prediction
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
+    const { decodedToken, error } = await firebaseAuthMiddleware(request);
+
+    if (error || !decodedToken) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const uid = emailToId(decodedToken.email || "");
+
+    const userRole = (await db.collection("users").doc(uid).get()).data()?.role;
+
+    if (
+      !scoreAdmin.email.includes(decodedToken.email || "") &&
+      userRole !== "admin"
+    ) {
+      return NextResponse.json(
+        { error: "You cannot access this route" },
+        { status: 403 }
+      );
+    }
+
     const predictions = (await db.collection("predictions").get()).docs;
 
     return NextResponse.json(
@@ -47,6 +70,26 @@ export async function GET() {
 //this is a method to add new prediction
 export async function POST(request: NextRequest) {
   try {
+    const { decodedToken, error } = await firebaseAuthMiddleware(request);
+
+    if (error || !decodedToken) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const uid = emailToId(decodedToken.email || "");
+
+    const userRole = (await db.collection("users").doc(uid).get()).data()?.role;
+
+    if (
+      !scoreAdmin.email.includes(decodedToken.email || "") &&
+      userRole !== "admin"
+    ) {
+      return NextResponse.json(
+        { error: "You cannot access this route" },
+        { status: 403 }
+      );
+    }
+
     const body = await request.json();
     const { question, solution, day, time } = body;
 
