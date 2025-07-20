76a2ec626034a5cbd47abe4cd23e0a13ff697a64
MildlyBrain <maethaphan.phai@gmail.com>
2025-07-20 18:50:46 +0700

    feat: reused Nuna's component

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,79 +1,44 @@
 "use client";
 
 import Button from "@/components/Button";
-import Icon from "@/components/Icon";
 import cn from "@/lib/helpers/cn";
 import { StyleableFC } from "@/lib/types/misc";
-import { motion } from "motion/react";
-import { useTranslations } from "next-intl";
-
-type DialogAction = "close" | "cancel" | "confirm";
 
 const Dialog: StyleableFC<{
-  icon: string;
-  title: string;
-  variant: "close" | "cancelOkay";
   children: React.ReactNode;
-  onAction: (action: DialogAction) => void;
-}> = ({ icon, title, variant, className, children, onAction }) => {
-  const t = useTranslations("Common.Action");
-
+  onClose?: () => void;
+  onConfirm?: () => void;
+}> = ({ children, onClose, onConfirm, className, style }) => {
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.25 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-        className="fixed inset-0 z-40 bg-black"
-        onClick={() => onAction(variant === "close" ? "close" : "cancel")}
-      />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-        className={cn(
-          "fixed inset-0 z-50 flex h-screen flex-col items-center justify-center",
-          className
-        )}
-      >
-        <motion.div className="bg-yellow-white fabric-texture flex w-[310px] flex-col items-center justify-center gap-4 p-6 pb-9">
-          <Icon name={icon} size={24} className="text-red" />
-          <h1 className="type-headline-small text-center text-balance text-black">
-            {title}
-          </h1>
-          {children}
-        </motion.div>
-        {variant === "close" ? (
-          <Button
-            className="w-[310px]"
-            Appearance="primary"
-            Size="medium"
-            onClick={() => onAction("close")}
-          >
-            <p className="type-title-medium">{t("close")}</p>
-          </Button>
-        ) : (
-          <div className="grid w-[310px] grid-cols-2">
-            <Button
-              Appearance="secondary"
-              Size="medium"
-              onClick={() => onAction("cancel")}
-            >
-              <p className="type-title-medium">{t("cancel")}</p>
-            </Button>
-            <Button
-              Appearance="primary"
-              Size="medium"
-              onClick={() => onAction("confirm")}
-            >
-              <p className="type-title-medium">{t("confirm")}</p>
-            </Button>
-          </div>
-        )}
-      </motion.div>
-    </>
+    <div
+      className={cn(
+        "bg-yellow-white fabric-texture relative w-[310px] overflow-hidden",
+        className
+      )}
+      style={style}
+    >
+      <div className="flex flex-col items-center justify-center gap-4 px-6 pt-6 pb-9">
+        {children}
+      </div>
+      <div className="flex w-full">
+        <Button
+          Size="medium"
+          Appearance="secondary"
+          onClick={onClose}
+          className="w-full"
+        >
+          <span className="type-title-medium">ยกเลิก</span>
+        </Button>
+        <Button
+          Size="medium"
+          Appearance="primary"
+          onClick={onConfirm}
+          className="w-full"
+        >
+          <span className="type-title-medium">ยืนยัน</span>
+        </Button>
+      </div>
+    </div>
   );
 };
 
