20522de37c4f75f9ad4e6750d004676023c867b2
nuna101 <nunajidapanak@gmail.com>
2025-07-22 06:58:16 +0700

    feat: prediction history

--- src/app/[locale]/games/predictions/histories/components/HistoryCard.tsx
+++ src/app/[locale]/games/predictions/histories/components/HistoryCard.tsx
@@ -1,52 +1,50 @@
-import Icon from "@/components/Icon";
+"use client";
+
 import cn from "@/lib/helpers/cn";
 import { StyleableFC } from "@/lib/types/misc";
+import type { PredictionHistoryItem } from "@/lib/types/prediction";
+import { useLocale, useTranslations } from "next-intl";
 
-type HistoryProps = {
-  time?: string;
-  question?: string;
-  userAnswer?: string;
-  correctAnswer?: string;
-  isCorrect?: boolean;
-};
-
-const HistoryCard: StyleableFC<{ history: HistoryProps }> = ({
+const HistoryCard: StyleableFC<{ history: PredictionHistoryItem }> = ({
   history,
   className,
   style,
 }) => {
+  const locale = useLocale() as "th" | "en";
+  const t = useTranslations("Predictions.PredictionCard");
+  const time = history?.time === "เช้า" ? "morning" : "afternoon";
   return (
     <div
       className={cn(
-        "bg-blue relative flex h-28 w-full flex-col gap-3 p-4",
+        "bg-blue relative flex w-full flex-col gap-3 p-4",
         className
       )}
       style={style}
     >
-      <div
-        className={cn(
-          "absolute inset-0 bg-[url('/decorating/texture/fabric.png')] opacity-50 mix-blend-soft-light"
-        )}
-      />
+      <div className={cn("fabric-texture absolute inset-0")} />
       <div>
-        <p className="type-title-medium text-yellow">{history.time}</p>
-        <p className="type-body-medium text-white">{history.question}</p>
+        <p className="type-title-medium text-yellow">
+          {t("round", { day: history.day, time })}
+        </p>
+        <p className="type-body-medium text-white">
+          {history.question[locale]}
+        </p>
       </div>
       <div className="flex justify-between">
         <div>
           <span className="type-title-medium pr-2 text-white">
-            {history.userAnswer}
+            {history.answer}
           </span>
-          {!history.isCorrect && (
+          {/* {!history.isCorrect && (
             <span className="type-title-medium text-yellow">
-              เฉลย: {history.correctAnswer}
+              เฉลย: {history.solution[locale]}
             </span>
-          )}
+          )} */}
         </div>
-        <Icon
+        {/* <Icon
           name={history.isCorrect ? "check" : "close"}
           className="text-white"
-        />
+        /> */}
       </div>
     </div>
   );
