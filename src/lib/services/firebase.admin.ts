bc7ee3560909ff218dc0637eeba9771bccc3e654
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-07-21 11:25:40 +0700

    fix: use NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET

--- src/lib/services/firebase.admin.ts
+++ src/lib/services/firebase.admin.ts
@@ -10,7 +10,7 @@ export function getFirebaseAdmin() {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      storageBucket: process.env.FB_STORAGE_BUCKET,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   }
 
