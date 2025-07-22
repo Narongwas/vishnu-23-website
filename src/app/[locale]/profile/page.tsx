8d474c9d9d788bb9fcfb03ec6d1fa0e3be984252
MildlyBrain <maethaphan.phai@gmail.com>
2025-07-22 10:39:29 +0700

    feat: add friend coming soon

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,9 +1,16 @@
 import UserProfileInfo from "@/app/[locale]/profile/components/UserProfileInfo";
+import Icon from "@/components/Icon";
 //import FriendList from "@/app/[locale]/profile/components/friendList";
 export default function ProfilePage() {
   return (
     <div className="flex h-screen flex-col gap-6">
       <UserProfileInfo />
+      <div className="flex w-92.5 flex-col items-center justify-center gap-5 self-center px-7.5 py-24 text-white">
+        <Icon name="acute" size={40} />
+        <h1 className="type-headline-small text-center text-balance">
+          ฟีเจอร์เพิ่มเพื่อนกำลังจะมาเร็ว ๆ นี้
+        </h1>
+      </div>
       {/* <FriendList /> */}
     </div>
   );
