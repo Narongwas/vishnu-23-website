fbfc5439e787ee312c170951895f21c354f417cb
frostnzx <jeensiwakorn001@gmail.com>
2025-07-22 21:58:05 +0700

    fix: protect game page

--- src/app/[locale]/games/layout.tsx
+++ src/app/[locale]/games/layout.tsx
@@ -1,15 +1,20 @@
 import BackgroundWithNoise from "@/components/BackgroundWithNoise";
+import { checkUserAccess } from "@/lib/middleware/checkUserAccess";
 import type { Viewport } from "next";
+import { redirect } from "next/navigation";
 
 export const viewport: Viewport = {
   themeColor: "#41415f",
 };
 
-export default function GamesLayout({
+export default async function GamesLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (!(await checkUserAccess(["camper", "admin"]))) {
+    redirect("/");
+  }
   return (
     <BackgroundWithNoise className="bg-blue from-blue bg-linear-to-b to-white/30">
       <div className="relative mx-auto min-h-screen max-w-200 overflow-hidden px-4">
